878f8d7aa7fc8cb27c8aa60c090a1913ce38bd2b
JoarVarpe <joar.varpe@gmail.com>
2020-07-20 20:28:08 +0200

    add some more exercises

---
+++ chapter_10_exercises.py
@@ -0,0 +1,166 @@
+import turtle, math
+
+window = turtle.Screen()
+abra = turtle.Turtle()
+
+
+def koch(tortoise: turtle.Turtle(), order: int, size: int) -> None:
+    """
+    make the turtle tortoise draw a Koch fractal of 'order' and 'size'.
+    Leave the turtle facing the same direction.
+    """
+    if order == 0:
+        tortoise.forward(size)
+    else:
+        for angle in [60, -120, 60, 0]:
+            koch(tortoise, order - 1, size / 3)
+            tortoise.left(angle)
+
+
+def koch_snowflake(tortoise: turtle.Turtle(), order: int, size: int) -> None:
+    """
+    Make a snowflake of Koch fractals of 'order' and 'size'.
+    Leave the turtle facing start direction.
+    """
+    tortoise.hideturtle()
+    for angle in [-120, -120, -120]:
+        koch(tortoise, order, size)
+        tortoise.left(angle)
+
+
+# TODO revisit cesaro so that the lengths are the same with increasing order....
+def cesaro(tortoise: turtle.Turtle(), order: int, size: float, angle_tear: int) -> None:
+    """
+    Make a cesaro torn line fractal of 'order', 'size' and 'tear angle' given by the user
+    """
+    experimental_list = [angle_tear / 2 - 90, -180 - angle_tear, - 90 + angle_tear / 2, 0]
+    if order == 0:
+        tortoise.forward(size)
+    else:
+
+        for angle in experimental_list:
+            cesaro(tortoise, order - 1, size / 2, angle_tear)
+            tortoise.left(angle)
+    # else:
+    #     for angle in experimental_list:
+    #         cesaro(tortoise, order - 1, size / 2, angle_tear)
+    #         tortoise.left(angle)
+
+
+def cesaro_square(tortoise: turtle.Turtle(), order: int, size: int, angle_tear: int) -> None:
+    """
+    Make a cesaro torn line fractal square of 'order', 'size' and 'tear angle' given by the user
+    """
+    for angle in [-90, -90, -90, -90]:
+        cesaro(tortoise, order, size, angle_tear)
+        tortoise.left(angle)
+
+
+def test_cesaros():
+    turtle_size = 10
+    abra.speed(20)
+    list_of_cords = [window.window_height() / 2 - turtle_size / 2,
+                     window.window_height() / 2 - turtle_size / 2 - 75,
+                     window.window_height() / 2 - turtle_size / 2 - 150,
+                     window.window_height() / 2 - turtle_size / 2 - 225]
+
+    for i in range(len(list_of_cords)):
+        abra.penup()
+        abra.goto(turtle_size / 2 - window.window_width() / 2, list_of_cords[i])
+        abra.pendown()
+        cesaro(abra, i, 100, 10)
+
+
+def make_triangle(tortoise: turtle.Turtle(), size: float) -> None:
+    for angle in [120, 120, 120]:
+        tortoise.forward(size)
+        tortoise.left(angle)
+
+
+# TODO Try to make the triangles total outer area the same for each
+def sierpinski(tortoise: turtle.Turtle(), order: int, size: float) -> None:
+    print(order)
+    if order == 0:
+        make_triangle(tortoise, size)
+    else:
+
+        sierpinski(tortoise, order - 1, size / 3)
+        abra.penup()
+        abra.forward((size * ((2 ** (order - 1)) / (3 ** order))))
+        abra.pendown()
+        sierpinski(tortoise, order - 1, size / 3)
+        abra.penup()
+        abra.left(120)
+        abra.forward((size * ((2 ** (order - 1)) / (3 ** order))))
+
+        abra.left(-120)
+        abra.pendown()
+        sierpinski(tortoise, order - 1, size / 3)
+        tortoise.penup()
+        tortoise.left(-120)
+        abra.forward((size * ((2 ** (order - 1)) / (3 ** order))))
+
+        tortoise.left(120)
+        tortoise.pendown()
+
+
+list_of_colors = ["red", "green", "orange"]
+
+
+def next_color(tortoise: turtle.Turtle()) -> None:
+    if tortoise.color()[0] not in list_of_colors:
+        tortoise.color(list_of_colors[0])
+    else:
+        try:
+            tortoise.color(list_of_colors[list_of_colors.index(tortoise.color()[0]) + 1])
+        except IndexError:
+            tortoise.color(list_of_colors[0])
+    print(tortoise.color())
+
+
+# TODO find a more elegant way of solving this one V
+def color_sierpinski(tortoise: turtle.Turtle(), order: int, size: float,
+                     starting_order: int, color_change_depth=-1) -> None:
+    if -1 < color_change_depth:
+        if (starting_order - order - 1) == color_change_depth:
+            next_color(tortoise)
+            print("changing color to {}".format(tortoise.color()[0]))
+        if order == 0:
+            make_triangle(tortoise, size)
+        else:
+
+            color_sierpinski(tortoise, order - 1, size / 3, starting_order, color_change_depth)
+            abra.penup()
+            abra.forward((size * ((2 ** (order - 1)) / (3 ** order))))
+            abra.pendown()
+            color_sierpinski(tortoise, order - 1, size / 3, starting_order, color_change_depth)
+            abra.penup()
+            abra.left(120)
+            abra.forward((size * ((2 ** (order - 1)) / (3 ** order))))
+            abra.left(-120)
+            abra.pendown()
+            color_sierpinski(tortoise, order - 1, size / 3, starting_order, color_change_depth)
+            tortoise.penup()
+            tortoise.left(-120)
+            abra.forward((size * ((2 ** (order - 1)) / (3 ** order))))
+            tortoise.left(120)
+            tortoise.pendown()
+    else:
+        sierpinski(tortoise, order, size)
+
+
+abra.speed(30)
+# sierpinski(abra, 1, 500)
+# abra.forward(500 / 3)
+# sierpinski(abra, 2, 500)
+# abra.forward(500 / 9)
+# sierpinski(abra, 3, 500)
+# abra.forward(500 * 4 / 27)
+# sierpinski(abra, 4, 500)
+# abra.forward(500 * 8 / 54)
+# sierpinski(abra, 5, 500)
+color_sierpinski(abra, 5, 3000, 5, 0)
+# next_color(abra)
+window.mainloop()
+
+def recursive_min(nested_number_list)
