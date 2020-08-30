1c31f9fe847f369867d6e8cb650c7b9caf7ba1c0
JoarVarpe <joar.varpe@gmail.com>
2020-08-30 12:06:03 +0200

    added the exercises on chapter 15

--- chapter_15_exercises.py
+++ chapter_15_exercises.py
@@ -1,2 +1,6 @@
 from matplotlib import pyplot as plt
-plt.plot([1, 2, 3, 4])
+# Dashed line
+plt.plot([1, 2, 3, 4], [0.1, 0.2, 0.3, 0.4], "b--")
+# Plotmarkers on all datapoints
+plt.plot([1, 4, 9, 16], [0.1, 0.2, 0.3, 0.4], "r.")
+plt.show()
